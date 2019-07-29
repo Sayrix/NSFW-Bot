ea3665a45a1427eb8a0d399676e1acb28cc9eeb5
Sayrix <43046854+Sayrixytfz@users.noreply.github.com>
2019-07-29 22:58:24 +0200

    Update ready.js

--- handlers/ready.js
+++ handlers/ready.js
@@ -4,15 +4,11 @@ const moment = require('moment')
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is ready for action!`);
-    bot.guilds.get('592746774171484172').channels.get('593012689249501187').send(`[${moment().format('DD/MM/YY - hh:mm:ss')}] Je viens de me connecter !`)
     bot.user.setActivity()
 
     let status_liste = [
-      `n/help`,
-      `n/help | ${bot.guilds.size} Serveurs`,
-      `n/help | ${bot.users.size} Utilisateurs`,
-      `n/help | ${bot.channels.size} Channels`,
-      `n/help `
+      `game1,
+      `game2`
     ]
   
     setInterval(() => {
