afa6ec12fd71201a2c948c92236a0a30c362ab1b
Sayrix <43046854+Sayrix@users.noreply.github.com>
2020-08-08 20:55:41 +0200

    🔧 Updated to Discord.JS 12
    🔧 Updated to Discord.JS 12

--- cmd/help.js
+++ cmd/help.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = (client, message, args) => {
-    var help = new Discord.RichEmbed()
+    var help = new Discord.MessageEmbed()
     .setAuthor('Help :')
     .setDescription('Prefix : n!\nLa liste des commandes :')
     .addField('NSFW  :underage: :', '`4k`, `anal`, `ass`, `hentai`, `hkitsune`, `hneko`, `holo`, `kemonomimi`, `neko`, `pussy`')
