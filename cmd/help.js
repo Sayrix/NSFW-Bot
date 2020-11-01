d667e23e6fedded8d25d2357bd07cad5c22b0ade
Sayrix <43046854+Sayrix@users.noreply.github.com>
2020-11-01 15:28:22 +0100

    Delete help.js

--- cmd/help.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-module.exports.run = (client, message, args) => {
-    var help = new Discord.MessageEmbed()
-    .setAuthor('Help :')
-    .setDescription('Prefix : n!\nLa liste des commandes :')
-    .addField('NSFW  :underage: :', '`4k`, `anal`, `ass`, `hentai`, `hkitsune`, `hneko`, `holo`, `kemonomimi`, `neko`, `pussy`')
-    .addField('Autre :', '`help`, `stats`, `support`, `invite`')
-    message.channel.send(help)
-}
