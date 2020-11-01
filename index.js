e0cbe89ea59257bd6cbb9c69992140038a4ff646
Sayrix <43046854+Sayrix@users.noreply.github.com>
2020-11-01 15:26:27 +0100

    🔧 Updated to Discord.js 12

---
+++ index.js
@@ -0,0 +1,31 @@
+const discord = require('discord.js'),
+config = require("./config.json"),
+fs = require('fs'),
+moment = require('moment'),
+superagent = require('superagent'),
+os = require('os');
+
+moment.locale('fr');
+
+const client = new discord.Client();
+client.commands = new discord.Collection();
+client.queue = new Map();
+client.config = config;
+
+client.on('ready', function() {
+    console.log("Launched")
+});
+
+client.on('message', function(message) {
+  if (message.author.bot) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  const cmd = client.commands.get(command);
+  if (!cmd) return;
+  cmd.run(client, message, args);
+});
+
+fs.readdir("./cmd/", (err, files) => {if (err) return console.error(err);files.forEach(file => {if (!file.endsWith(".js")) return;let props = require(`./cmd/${file}`);let commandName = file.split(".")[0];console.log(`Load command ${commandName}`);client.commands.set(commandName, props);});});
+
+client.login(config.token)
